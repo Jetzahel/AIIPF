caf991fc208ed8526025b7f01cc9fc77bc1b8e7c
Joel Tello <jetzahel@gmail.com>
2016-10-11 23:29:27 -0500

    Creacion de ficheros base

---
+++ src/aii_p1/AL1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aii_p1;
+import java.io.*;
+
+/**
+ *
+ * @author Joel
+ */
+public class AL1 {
+    
+    
+}
